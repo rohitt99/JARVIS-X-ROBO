7e6ebcd4f248e571592ecefd142e25df84329071
Rohit <164199173+rohitt99@users.noreply.github.com>
2024-08-06 12:02:33 +0530

    Update tagall.py

--- JarvisRobo/modules/tagall.py
+++ JarvisRobo/modules/tagall.py
@@ -5,7 +5,7 @@ from telethon.errors import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 
-from Ava import telethn as tbot
+from JarvisRobo import telethn as tbot
 
 spam_chats = []
 
